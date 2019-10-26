8ec98e13b5fad967da8c4aee4fa9205a006c0e6a
Karinka <kharinakaryna@gmail.com>
2019-10-26 18:45:51 +0300

    springboot app

--- src/main/java/springboot/controller/GreetingController.java
+++ src/main/java/springboot/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package springboot;
+package springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- src/main/java/springboot/repos/MessageRepos.java
+++ src/main/java/springboot/repos/MessageRepos.java
@@ -3,6 +3,5 @@ package springboot.repos;
 import org.springframework.data.repository.CrudRepository;
 import springboot.domain.Message;
 
-
 public interface MessageRepos extends CrudRepository<Message, Long> {
 }
